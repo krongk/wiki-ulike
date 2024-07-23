4be2a1be0f4a12de45e21088f2d7f8e34ad23d98
xuejiang <song@tanmer.com>
2024-07-23 20:45:01 +0800

    add first

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import * as Turbo from "@hotwired/turbo"
+import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
+import { Application } from "@hotwired/stimulus"
+import MenuController from "./menu_controller"
+import ViewImagesController from "./view_images_controller"
+import NavtreeController from "./navtree_controller"
+import ThemeController from "./theme_controller"
+import Dropdown from 'stimulus-dropdown'
+
+window.Alpine = Alpine
+Alpine.plugin(collapse)
+Alpine.start()
+
+const application = Application.start()
+window.Stimulus = application
+application.register('menu', MenuController)
+application.register('view_images', ViewImagesController)
+application.register('navtree', NavtreeController)
+application.register('theme', ThemeController)
+application.register('dropdown', Dropdown)
