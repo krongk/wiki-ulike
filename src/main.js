1a4adb8c030a1922f2493b3cebae4d09e33c6515
李成红(lichenghong@baklib.vip) <1959507363@qq.com>
2025-10-15 10:20:09 +0800

    修复导航问题

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
 import { Application } from "@hotwired/stimulus"
 import MenuController from "./menu_controller"
-import RecaptchaController from "./recaptcha_controller"
 import ViewImagesController from "./view_images_controller"
-import NavtreeController from "./navtree_controller"
+// import NavtreeController from "./navtree_controller"
 // import ThemeController from "./theme_controller"
 import Dropdown from 'stimulus-dropdown'
 import Clipboard from '@stimulus-components/clipboard'
 
+import TurboNavTreeController from "./turbo_nav_tree_controller"
 
 window.Alpine = Alpine
 Alpine.plugin(collapse)
@@ -19,8 +19,7 @@ const application = Application.start()
 window.Stimulus = application
 application.register('menu', MenuController)
 application.register('view_images', ViewImagesController)
-application.register('navtree', NavtreeController)
 // application.register('theme', ThemeController)
 application.register('dropdown', Dropdown)
 application.register('clipboard', Clipboard)
-application.register('recaptcha', RecaptchaController)
+application.register('turbo-nav-tree', TurboNavTreeController)
